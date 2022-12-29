353938c50df03a5ecf4615333e1b5e387957609a
b-shivam022 <113627157+b-shivam022@users.noreply.github.com>
2022-12-29 11:39:12 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const express = require("express");
+const cord = require("")
 const app = express();
 const useRouter = require("./server/routes/router");
 const path = require("path");
@@ -17,6 +18,8 @@ mongoose.connect(DB).then(() => {
     console.log("no connection");
   });
 
+app.use(cors());  
+
 app.use("/", useRouter);
 
 app.use(express.static(path.join(__dirname,'./client/build')))
@@ -25,7 +28,7 @@ app.get('*', (req, res) => {
   res.send(path.join(__dirname,"./client/build/index.html"));
 });
 
-const PORT = 5000 || process.env.PORT;
+const PORT = 8080 || process.env.PORT;
 
 app.listen(PORT, () => {
   console.log("server is running on port 5000");
